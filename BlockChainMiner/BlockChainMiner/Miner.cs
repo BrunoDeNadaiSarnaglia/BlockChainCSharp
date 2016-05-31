a0bfaaa1f08df7cfd93ccea31988b27203605d97
BrunoDeNadaiSarnaglia <brunodenadaisarnaglia@gmail.com>
2016-05-31 02:43:55 -0300

    Sha and Miner interface done

---
+++ BlockChainMiner/BlockChainMiner/Miner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BlockChainMiner
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Miner : IMiner
+    {
+        #region Private Members
+
+        private ISha hash;
+        private ISha Hash
+        {
+            get { return this.hash; }
+            set { this.hash = value; }
+        }
+
+        private IEnumerable<IMiner> links;
+        private IEnumerable<IMiner> Links 
+        {
+            get { return this.links; } 
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Miner(IEnumerable<IMiner> links)
+        {
+            this.links = links;
+        }
+
+        #endregion
+
+        public void BroadcastHash(ISha sha)
+        {
+            foreach (IMiner miner in this.Links)
+            {
+                miner.ReceiveHash(sha);
+            }
+        }
+
+        public void ReceiveHash(ISha sha)
+        {
+            if (this.Hash.Value == sha.Value) return;
+            BroadcastHash(sha);
+        }
+
+    }
+}
---
+++ BlockChainMiner/BlockChainMiner/Sha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainMiner
+{
+    [DataContract]
+    public class Sha : ISha
+    {
+        #region Constructor
+        public Sha(string value)
+        {
+            this.value = value;
+        }
+
+        #endregion
+
+
+        [DataMember]
+        private string value;
+        public string Value {
+            get { return this.value; }
+        }
+    }
+}
